a41fa511e3a02bd3613fccb04342c9c68a6baf9b
Blue_sky303 <blue_sky303@qq.com>
2025-04-15 11:56:24 +0800

    好像还是得ensureascii

--- plugins/simple_chat/deepseek/deepseek.py
+++ plugins/simple_chat/deepseek/deepseek.py
@@ -22,7 +22,7 @@ async def group_chat(group_id, message, name, time):
         print(re)
         messages.append(re)
         messages_info[group_id] = messages
-        json.dump(messages_info, open(path.parent / "messages.json", "w"), ensure_ascii=False)
+        json.dump(messages_info, open(path.parent / "messages.json", "w"))
         r = json.loads(re['content'])
         print(r)
         if r['state'] == '发送':
